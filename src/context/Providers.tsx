bcc8f9ccb2e45f7af98f77a6f14ff8dc3dde0536
Md Asharaf <718romanempire@gmail.com>
2026-08-24 20:13:53 +0530

    move QueryClient instantiation to useState to ensure unique client per session and update default query options

--- src/context/Providers.tsx
+++ src/context/Providers.tsx
@@ -1,23 +1,28 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const client = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60000,
-      refetchOnWindowFocus: true,
-      refetchOnMount: true,
-      refetchOnReconnect: true,
-      retry: 1,
-      refetchInterval: false,
-      refetchIntervalInBackground: false,
-    },
-  },
-});
+import { useState } from "react";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 60 * 1000,
+            refetchOnWindowFocus: false,
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            retry: 1,
+            refetchInterval: false,
+            refetchIntervalInBackground: false,
+          },
+        },
+      })
+  );
+
   return (
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       {children}
     </QueryClientProvider>
   );
